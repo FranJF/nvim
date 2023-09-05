b3f546b23d8c4a7d08a7ae3f0297b8e120da7c36
FranJF <franjcfernandez@gmail.com>
2023-09-05 19:28:35 +0200

    improved: lazy branch

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,4 +1,13 @@
 return {
+
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      { "<leader>gc", false },
+      { "<leader>gl", "<cmd>Telescope git_commits<cr>", desc = "Commits" },
+    },
+  },
+
   "tpope/vim-fugitive",
   {
     "max397574/better-escape.nvim",
--- lua/plugins/telescope.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "nvim-telescope/telescope.nvim",
-  keys = {
-    { "<leader>gc", false },
-    { "<leader>gl", "<cmd>Telescope git_commits<cr>", desc = "Commits" },
-  },
-}
